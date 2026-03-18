02b3765fd3c863474e12c5ab07e8c9da0f6b138e
MzokhulayoEC1 <mzokhulayo@ec1.co.za>
2026-03-19 00:01:42 +0200

    second push_server running

--- main.tf
+++ main.tf
@@ -5,8 +5,37 @@ provider "aws" {
 resource "aws_instance" "my_book_instance" {
     ami             = "ami-0c02fb55956c7d316"
     instance_type   =  "t3.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup python3 -m http.server 8080 &
+              EOF
+
+user_data_replace_on_change = true
 
 tags = {
   Name = "my_book_instance"
 }
+}
+
+#To allow the EC2 Instance to receive traffic on port 8080
+
+resource "aws_security_group" "instance" {
+    name = "my_book_instance"
+
+    ingress  {
+        from_port   =8080
+        to_port     =8080
+        protocol    ="tcp"
+        cidr_blocks  =["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
