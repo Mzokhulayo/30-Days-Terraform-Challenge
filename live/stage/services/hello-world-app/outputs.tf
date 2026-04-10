40c9e15f39a6c6f00587ebf08ca25739a31d203d
Mzokhulayo <nkululeko.mdubeki@hotmail.com>
2026-04-10 16:05:44 +0200

    Add instance security group ID output for Day 21

--- live/stage/services/hello-world-app/outputs.tf
+++ live/stage/services/hello-world-app/outputs.tf
@@ -2,3 +2,8 @@ output "alb_dns_name" {
   value       = module.Hello_world_app.alb_dns_name
   description = "The domain name of the load balancer"
 }
+
+output "instance_security_group_id" {
+  value       = module.Hello_world_app.instance_security_group_id
+  description = "The ID of the EC2 Instance Security Group"
+}
